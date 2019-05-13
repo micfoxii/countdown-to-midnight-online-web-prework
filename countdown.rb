e071c36260016fab124db3b35c5e6b891b39f6ef
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-13 16:24:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,17 @@
 #write your code here
-counter=int 
-  while counter == int
-  puts "#{counter} SECOND(S)!"
-  counter -= 1
+def countdown(number=int)
+  counter = 0 
+  while number > 0
+  puts "#{number} SECOND(S)!}"
+  number -=1 
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number=int)
+  counter =0
+  while number > 0 
+  sleep(1)
+  number -=1 
 end
 end
